c51f03f92b8456b4dc31f1742a7af12a45b03c14
Kevin Salazar <deeiqh@gmail.com>
2023-02-07 19:05:30 -0500

    chore: improve types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
+export interface IProvidersConfig {
+  cacheConfig: CacheManagerOptions;
+  kafkaConfig: KafkaOptions;
+}
 export class CommonModule {
   static forRoot(configs: IProvidersConfig): DynamicModule;
 }
