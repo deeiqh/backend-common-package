5cdba76c09435f706bb97b368d11eaef629bd307
Kevin Salazar <deeiqh@gmail.com>
2023-02-14 10:00:38 -0500

    chore: add model and payload reducer methods to common service

--- src/common.service.ts
+++ src/common.service.ts
@@ -2,8 +2,30 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CommonService {
-  //payload?.value?.payload?.userId?.value
-  //payload?.value?.payload?.customerUserEmail?.props?.email
+  reducePayload(payload: Record<string, any>): Record<string, any> {
+    //examples:
+    //  payload?.value?.payload?.userId?.value
+    //    => payload?.useId
+    //  payload?.value?.payload?.customerUserEmail?.props?.email
+    //    => payload?.customerUserEmail.email
+
+    if (!payload) return {};
+
+    if (payload.value) {
+      if (payload.value.payload) {
+        if (typeof payload.value.payload !== 'object') {
+          payload['value'] = payload.value.payload;
+        } else {
+          this.reduceModel(payload, payload.value.payload);
+        }
+      } else {
+        return payload.value;
+      }
+    }
+
+    return payload;
+  }
+
   reduceModel(
     model: Record<string, any>,
     fromProperty?: Record<string, any>,
@@ -11,6 +33,7 @@ export class CommonService {
     if (!fromProperty) {
       fromProperty = model;
     }
+
     for (const [property] of Object.keys(fromProperty)) {
       if (fromProperty[property].value) {
         model[property] = fromProperty[property].value;
@@ -26,80 +49,7 @@ export class CommonService {
         model[property] = fromProperty[property];
       }
     }
+
     return model;
-  }
-
-  reducePayload(payload: Record<string, any>): Record<string, any> {
-    if (!payload) return {};
-    if (payload.value) {
-      if (payload.value.payload) {
-        if (typeof payload.value.payload !== 'object') {
-          payload['value'] = payload.value.payload;
-        } else {
-          this.reduceModel(payload, payload.value.payload);
-          // for (const [property] of Object.keys(payload.value.payload)) {
-          //   if (payload.value.payload[property].value) {
-          //     payload[property] = payload.value.payload[property].value;
-          //   } else if (payload.value.payload[property].props) {
-          //     for (const [prop, value] of Object.keys(
-          //       payload.value.payload.props,
-          //     )) {
-          //       payload[property][prop] = value;
-          //     }
-          //   } else if (typeof payload.value.payload[property] === 'object') {
-          //     for (const [key, value] of Object.keys(
-          //       payload.value.payload[property],
-          //     )) {
-          //       payload[property][key] = value;
-          //     }
-          //   } else {
-          //     payload[property] = payload.value.payload[property];
-          //   }
-          // }
-        }
-      } else {
-        return payload.value;
-      }
-    }
-    return payload;
-
-    //payload?.value?.payload?.userId?.value
-    //payload?.value?.payload?.customerUserEmail?.props?.email
-
-    // let lastValue;
-    // let lastModel: Record<string, unknown>;
-    // let lastKey;
-
-    // let reducedModel: Record<string, unknown> = {};
-    // let current = model;
-    // let value: any
-    // while (current !== null) {
-    //   value = current
-    //   if (typeof value === 'object') {
-    //     if (['payload', 'value', 'props'].find((e) => e === key)) {
-    //       continue;
-    //     } else {
-    //       reducedModel = value;
-    //     }
-    //   } else {
-    //     reducedModel[key] = value;
-    //   }
-
-    //   // switch (key) {
-    //   //   case 'payload':
-    //   //     break;
-    //   //   case 'value':
-    //   //     lastValue = value;
-    //   //     break;
-    //   //   default:
-    //   //     model[key] = value; //model.userId
-    //   //     lastModel = model[key];
-    //   //     lastKey = key;
-    //   // }
-    // }
-    // if (lastValue) {
-    //   lastModel[lastKey] = lastValue;
-    // }
-    // return model;
   }
 }
