625e87268ffbabd922262b7ffea1225b5b424795
Kevin Salazar <deeiqh@gmail.com>
2023-02-20 23:24:05 -0500

    chore: improve decorator

--- src/reduce-payload/decorators/reduce-payload.decorator.ts
+++ src/reduce-payload/decorators/reduce-payload.decorator.ts
@@ -1,8 +1,29 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+function warnIfPayloadIsAnyType(
+  payload: any,
+  payloads: Record<string, any>,
+  eventName: string,
+) {
+  if (!payloads[eventName]) {
+    console.log(
+      payload,
+      'EN LOGGER COMO WARNINGwrite to file. New Event Coded, please restart app.',
+    );
+  }
+}
+
+const payloads = { event1: '1' };
+
 export const ReducePayload = createParamDecorator(
   (data: any, context: ExecutionContext) => {
-    const request = context;
-    console.log('REQUEST', request, 'DAAATA', data);
+    const request = context.switchToHttp().getRequest();
+
+    const eventName = request.value.category;
+    const _payload = request.value.payload;
+
+    warnIfPayloadIsAnyType(_payload, payloads, eventName);
+
+    return _payload;
   },
 );
